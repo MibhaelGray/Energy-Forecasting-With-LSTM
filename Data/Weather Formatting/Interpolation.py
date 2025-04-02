61ef5f1baa62954c5e7fa3b711c0f8409888a134
MibhaelGray <mgray.gray4173@gmail.com>
2025-04-01 19:01:13 -0700

    Commit

--- Data/Weather Formatting/Interpolation.py
+++ Data/Weather Formatting/Interpolation.py
@@ -1,2 +1,10 @@
 import pandas as pd
 
+# Importing the data
+filtered_weather_data = pd.read_csv("C:/Users/mgray/OneDrive/Documents/Energy Forecasting/Data/Weather Data/filtered_weather_data.csv")
+print(filtered_weather_data.head())
+
+
+# We need to find how many rows have missing values
+
+print(filtered_weather_data.isnull().sum())
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # SARIMAX-LSTM Hybrid Model
 
 
+
+
 ## SARIMAX
 1. Air Temp
 2. Wind Generation
 3. Solar Generation
 4. Oil and gas production
+
+## Daily Goals
+
+1. Collect Air temp, Wind Generation, Solar Generation
